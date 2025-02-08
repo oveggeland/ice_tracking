bbcf5c2d89779409ef31d26215ac2fafaab39a39
Oskar <oveggeland@gmail.com>
2025-02-08 18:51:31 +0100

    FrameBuffer clean up

--- src/icetrack/frontend/FrameBuffer.h
+++ src/icetrack/frontend/FrameBuffer.h
@@ -1,20 +1,14 @@
 #pragma once
 
 #include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/point_cloud2_iterator.h>
 
 #include <gtsam/geometry/Pose3.h>
-
-#include <open3d/Open3D.h>
-#include <open3d/geometry/PointCloud.h>
+#include <open3d/t/geometry/PointCloud.h>
 
 #include "frontend/PointBuffer.h"
 #include "backend/PoseGraph.h"
 
-#include "utils/calibration.h"
 #include "utils/ros_params.h"
-#include "utils/StampedRingBuffer.h"
 #include "utils/pointcloud.h"
 
 using FrameType = TensorCloudPtr;
@@ -28,11 +22,9 @@ public:
     
     // Interface
     void generateFrames();
-    void addFrame(int idx);
 
     // Accessors
-    int getIndex() const { return end()->first; }
-    FrameType getFrame(int idx) const { 
+    const FrameType getFrame(int idx) const { 
         auto it = buffer_.find(idx);
         return (it != buffer_.end()) ? it->second : nullptr;
     }
@@ -44,14 +36,18 @@ public:
 private:
     FrameBufferType buffer_;
 
-    // Keep track of latest count
-    int frame_count_ = 0;
+    // Add frame with points between t_{idx-1} and t_{idx}
+    void addFrame(int idx);
 
-    bool undistort_ = true;
-    bool visualize_ = false;
-    int visualize_count_ = 5000;
+    // Remove out of scope frames
+    void removeOldFrames();
 
-    // Access
+    // Option to undistort frames by pose interpolation
+    bool undistort_frames_;
+
+    // Reference to buffer of incoming lidar points
     const PointBuffer& point_buffer_;
+
+    // Reference to posegraph object
     const PoseGraph& pose_graph_;
 };
