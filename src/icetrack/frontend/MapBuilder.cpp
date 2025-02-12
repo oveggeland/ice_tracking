fd16d2a59735f3b494e83e2431665d97d80484a3
Oskar <oveggeland@gmail.com>
2025-02-12 13:04:09 +0100

    Publisher for map

--- src/icetrack/frontend/MapBuilder.cpp
+++ src/icetrack/frontend/MapBuilder.cpp
@@ -3,6 +3,36 @@
 MapBuilder::MapBuilder(ros::NodeHandle& nh, const PoseGraph& pose_graph, const FrameBuffer& frame_buffer) 
     : pose_graph_(pose_graph), frame_buffer_(frame_buffer){
     // Optionally load parameters from ros server (using nodehandle)
+    cloud_pub_ = nh.advertise<sensor_msgs::PointCloud2>("map", 10);
+    initializeCloudMsg(nh);
+}
+
+void MapBuilder::initializeCloudMsg(const ros::NodeHandle& nh){
+    // Initialize the PointCloud2 message metadata
+    cloud_msg_.header.frame_id = "map";  // Adjust frame ID as needed
+    cloud_msg_.height = 1;
+    cloud_msg_.is_dense = true;
+    cloud_msg_.is_bigendian = false;
+
+    // Define point fields for x, y, z
+    cloud_msg_.fields.resize(3);
+
+    cloud_msg_.fields[0].name = "x";
+    cloud_msg_.fields[0].offset = 0;
+    cloud_msg_.fields[0].datatype = sensor_msgs::PointField::FLOAT32;
+    cloud_msg_.fields[0].count = 1;
+
+    cloud_msg_.fields[1].name = "y";
+    cloud_msg_.fields[1].offset = 4;
+    cloud_msg_.fields[1].datatype = sensor_msgs::PointField::FLOAT32;
+    cloud_msg_.fields[1].count = 1;
+
+    cloud_msg_.fields[2].name = "z";
+    cloud_msg_.fields[2].offset = 8;
+    cloud_msg_.fields[2].datatype = sensor_msgs::PointField::FLOAT32;
+    cloud_msg_.fields[2].count = 1;
+
+    cloud_msg_.point_step = 12;  // 3 fields * 4 bytes per float
 }
 
 void MapBuilder::pollUpdates(){
@@ -13,11 +43,12 @@ void MapBuilder::pollUpdates(){
 
 void MapBuilder::updateMap(){
     last_frame_idx_ = frame_buffer_.getLastFrameIdx();
+    ts_map_ = pose_graph_.getTimeStamp(last_frame_idx_);
 
     int point_count = frame_buffer_.getPointCount();
+    ROS_WARN_STREAM("Update map with count " << point_count);
     if (point_count < 1)
         return;
-    ROS_INFO_STREAM("Building map of size: " << point_count);
 
     // Allocate space for transformed points arrays
     map_ = Eigen::MatrixXd(3, point_count);
@@ -39,12 +70,37 @@ void MapBuilder::updateMap(){
         map_.block(0, point_counter, 3, frame_size)  = (R * it->positions).colwise() + t;  // Eigen broadcasting!
         point_counter += frame_size;
     }
-    
-    // What now?
-    if (point_counter > 1e6)
-        visualizeMap();
+
+    publishAsPointCloud2();
 }
 
+void MapBuilder::publishAsPointCloud2() {
+    if (cloud_pub_.getNumSubscribers() < 1)
+        return;
+
+    if (map_.cols() == 0) {
+        ROS_WARN_THROTTLE(5, "Map is empty, skipping publish.");
+        return;
+    }
+
+    // Update message dimensions
+    cloud_msg_.width = map_.cols();
+    cloud_msg_.row_step = cloud_msg_.point_step * cloud_msg_.width;
+    cloud_msg_.data.resize(cloud_msg_.row_step);
+
+    // Fill in the point cloud data using iterators
+    sensor_msgs::PointCloud2Iterator<float> it(cloud_msg_, "x");
+    for (int i = 0; i < map_.cols(); ++i, ++it) {
+        // Set X, Y and Z
+        it[0] = map_(0, i);
+        it[1] = map_(1, i);
+        it[2] = map_(2, i);
+    }
+
+    // Update timestamp and publish
+    cloud_msg_.header.stamp = ros::Time(ts_map_);
+    cloud_pub_.publish(cloud_msg_);
+}
 
 /*
 Make a open3d cloud and visualize
--- src/icetrack/frontend/MapBuilder.h
+++ src/icetrack/frontend/MapBuilder.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
+
 #include "backend/PoseGraph.h"
 #include "frontend/FrameBuffer.h"
 
@@ -17,11 +20,19 @@ private:
     const PoseGraph& pose_graph_;
     const FrameBuffer& frame_buffer_;
 
+    double ts_map_;
     Eigen::MatrixXd map_;
 
-    int last_frame_idx_;
+    int last_frame_idx_ = 0;
     void updateMap();
 
     void visualizeMap();
+
+    // Publishing
+    ros::Publisher cloud_pub_;
+    sensor_msgs::PointCloud2 cloud_msg_;
+
+    void initializeCloudMsg(const ros::NodeHandle& nh);
+    void publishAsPointCloud2();
 };
 
